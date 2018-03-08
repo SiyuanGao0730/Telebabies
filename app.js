6978be4334b141cff4ec647d91838002d1cf58f8
garyzhao63 <garyzhao63@gmail.com>
2018-03-08 15:07:04 -0800

    Twilio, login, profile, settings
    use twilio to send text notification
    settings keep track of on/off button
    phone number is editable in profile

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ var familymembers = require('./routes/familymembers');
 var watson = require('watson-developer-cloud');
 var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
+/*** call the script that checks for twilio notificaiton **/
+require('./routes/routine-text.js');
+
 
 // Example route
 // var user = require('./routes/user');
@@ -104,8 +107,8 @@ app.get('/profile', profile.view);
 app.get('/record', record.view); 
 app.get('/result', result.view); 
 app.get('/result2', result.view2); 
-app.get('/result-history', resulthistory.view); 
-app.get('/result-history2', resulthistory.view2); 
+app.get('/result-history', resulthistory.view);
+app.get('/result-history2', resulthistory.view2);  
 app.get('/familymembers', familymembers.view);
 
 // Example route
@@ -127,7 +130,6 @@ app.post('/api/tone', function(req, res, next) {
 
 app.post('/wResult', function(req, res, next) {
 	let data = JSON.stringify(req.body); 
-	console.log(req.body); 
 	fs.writeFileSync('./result.json', data);  
 }); 
 
@@ -144,8 +146,7 @@ app.post('/rResult', function(req, res, next) {
 /***************** user list ***********/
 
 app.post('/wList', function(req, res, next) {
-  let data = JSON.stringify(req.body);  
-  console.log(data); 
+  let data = JSON.stringify(req.body);   
   fs.writeFileSync('./user_list.json', data);  
 });
 
@@ -157,12 +158,6 @@ app.get('/rList', function(req, res, next) {
       res.send(data); 
   });
 });
-
-
-
-
-
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
